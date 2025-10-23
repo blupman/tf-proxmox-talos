6ca328274bfd55f9cd7854f036156a5bcb048dad
root <root@talos-mgt.colokoe.net>
2025-10-24 00:17:46 +0200

    add more tools

--- setup-tools.sh
+++ setup-tools.sh
@@ -12,4 +12,14 @@ wget https://github.com/helmfile/helmfile/releases/download/v1.1.7/helmfile_1.1.
 tar zxvf helmfile_1.1.7_linux_amd64.tar.gz
 mv helmfile /usr/local/bin/q
 rm *
+echo talosctl
+wget https://github.com/siderolabs/talos/releases/download/v1.11.3/talosctl-linux-amd64
+mv talosctl-linux-amd64 /usr/local/bin/talosctl
+chmod +x /usr/local/bin/talosctl
+rm *
+echo kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin
+
 
